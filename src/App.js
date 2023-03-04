c5cb2ffc39f4c0a6dc6a861e7d113a4bb3b2960c
Alyson Rehm <ALY.REHM@gmail.com>
2023-03-03 19:51:01 -0800

    Axios Pokemon API Update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 import React, { useEffect, useState } from 'react';
+//updating to use axios 
+import axios from 'axios';
 
 function App() {
 
   const [pokemon, setPokemon] = useState([]);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
-      .then(response => {
-        return response.json()
+    // fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+      // .then(response => { return response.json()})
+      .then((response) => {
+        console.log(response.data.results);
+        setPokemon(response.data.results);
       })
-      .then(response => {
-
-        setPokemon(response.results)
-      })
+      // .then(response => {setPokemon(response.results)})
       .catch((err) => {
         console.log(err);
       })
-
+      // put empty array at the end of the useEffect for it to only run once when it is first opened 
   }, []);  
 
   return (
     <div className="App" style={{ width: "200px", margin: "auto" }}>
       <ul>
-        {
-          pokemon.map((pokemonObj, index)=>{
-            return (<li key={index}>{pokemonObj.name}</li>)
+        {pokemon.map((pokemonObject, index)=>{
+            return (<li key={index}>{pokemonObject.name}</li>)
           })
         }
       </ul>
